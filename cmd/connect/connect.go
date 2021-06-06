7867931e3c4bc3ce1265b8e8bc58f89c2be75e75
Sohail Khan <sohail@sohailkhan.me>
2021-06-05 20:27:31 -0700

    pass  struct to chan

--- cmd/connect/connect.go
+++ cmd/connect/connect.go
@@ -7,35 +7,40 @@ import "strings"
 import "log"
 import "io"
 
-func makeRequest(url string, channel chan string) {
+func makeRequest(url string, channel chan *Result) {
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal("unable to connect to: " + url)		
+		log.Fatal("unable to connect to: " + url)
 	}
-	
+
 	var body []byte
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("unable to read body for: " + url)		
+		log.Fatal("unable to read body for: " + url)
 	}
 
 	// fmt.Println(string(body))
-	channel <- string(body)
+	channel <- &Result{url, string(body)}
+}
+
+type Result struct {
+	Url      string
+	Response string
 }
 
 func main() {
 	prog, urls := os.Args[:1], os.Args[1:]
 	fmt.Printf("%s - %s\n", prog[0], strings.Join(urls, ", "))
-	queue := make(chan string, len(urls))
-	for _, url := range(urls) {
+	queue := make(chan *Result, len(urls))
+	for _, url := range urls {
 		go makeRequest(url, queue)
 	}
-	
+
 	var receivedCount = 0
-	var response string
+	var result *Result
 	for receivedCount < len(urls) {
-		response = <- queue	
-		fmt.Println(response[:10])
+		result = <-queue
+		fmt.Println(result.Url, result.Response[:10])
 		receivedCount += 1
 	}
 	close(queue)
